bf77db20bb36d0203dabaa1f3a54440b8b6a3d8c
Andrei Luis <1riphappy@gmail.com>
2026-06-27 13:38:59 +0800

    Update name variable to full name and uncomment additional string methods

--- Fundamentals/HelloWorld.py
+++ Fundamentals/HelloWorld.py
@@ -2,7 +2,7 @@ firstName, lastName, age, student = "Andrei Luis", "Nablo", 16, True
 
 Java = CPlusPlus = HtmlCss = GDScript = Arduino = RayLib = "Learned"
 
-name = "Andrei"
+name = "Andrei Luis Nablo"
 
 print(firstName)
 print(lastName)
@@ -16,4 +16,13 @@ print(GDScript)
 print(Arduino)
 print(RayLib)
 
-print(len(name))
+#print(len(name))
+#print(name.find("r"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("i"))
+print(name*3)
+
