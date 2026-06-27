5bf42235141e222f1daf51e916c225d90cd3c659
Andrei Luis <1riphappy@gmail.com>
2026-06-27 13:05:50 +0800

    Add HelloWorld.py with basic variable assignments and print statements

---
+++ Fundamentals/HelloWorld.py
@@ -0,0 +1,19 @@
+firstName, lastName, age, student = "Andrei Luis", "Nablo", 16, True
+
+Java = CPlusPlus = HtmlCss = GDScript = Arduino = RayLib = "Learned"
+
+name = "Andrei"
+
+print(firstName)
+print(lastName)
+print(age)
+print(student)
+
+print(Java)
+print(CPlusPlus)
+print(HtmlCss)
+print(GDScript)
+print(Arduino)
+print(RayLib)
+
+print(len(name))
