60898a29fb077e360615edab0ed6c41345d92aea
Andrei Luis <1riphappy@gmail.com>
2026-07-02 09:17:41 +0800

    Refactor shape calculators: remove Circle.py, Rectangle.py, and RightTriangle.py; add modular functions in Module.py and update PModule.py for testing

---
+++ Shapes/Module.py
@@ -0,0 +1,13 @@
+import math
+
+def area(radius):
+    return math.pi * pow(radius, 2)
+
+def circumfernce(radius):
+    return 2 * math.pi * radius
+
+def areaR(length, width):
+    return length * width
+
+def hypotenuse(a,b):
+    return math.sqrt(pow(a, 2) + pow(b, 2))
---
+++ Shapes/PModule.py
@@ -0,0 +1,6 @@
+import Module
+
+print(Module.area(5))
+print(Module.circumfernce(5))
+print(Module.areaR(7,5))
+print(Module.hypotenuse(15, 10))
